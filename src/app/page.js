a604f12f7a09f12e7a22c9b718353bb80a5f73cb
AntCross0 <thedurooo@gmail.com>
2023-09-24 18:22:27 -0400

    clip path hero section

--- src/app/page.js
+++ src/app/page.js
@@ -6,24 +6,19 @@ export default function Home() {
   return (
 
     <div className="relative w-full">
-
-      <div className='transition-all relative '>
-        <div className='relative w-full max-w-6xl overflow-clip bg-primary text-white p-2 md:p-12'>
+      <div className='min-w-screen transition-all relative paws-clip h-[512px] md:h-[738px]'>
+        <div className=' relative w-full h-[738px]  max-w-6xl overflow-clip bg-primary text-white p-2 md:p-12 paws-pattern'>
           <h2 className='transition-all z-10 h-fit px-2 font-black w-full sm:w-[26rem] line-clamp-2 py-2 leading-[4.2rem] text-5xl sm:text-6xl
           md:px-8  '>¿Guau can I help you?</h2>
           <p className='mt-4 font-bold w-full px-2 sm:w-[24rem] md:text-2xl md:w-[34rem] md:px-8 md:mt-8'>Get the app on the thing mom uses to point and
             laugh at me for no reason...</p>
           <div className='mt-4 px-2 md:px-8 md:mt-8'>
-
             <DownloadTabs />
           </div>
         </div>
-        <svg className='' viewBox="0 412 1280 340" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 0H1280V638C1280 638 1081 269.5 842 562C603 854.5 0 694 0 694L0 0Z" fill="#8A5FE8" />
-        </svg>
-        <Image className='z-0 transition-all absolute h-auto right-[7%] w-[300px] -bottom-[100%] sm:right-[15%] sm:-bottom-3/4 md:right-4 md:-bottom-36 lg:right-14 lg:-bottom-4' priority={false} src={doggy} alt='doggy doggy' />
-      </div>
 
+        <Image className='z-0 transition-all absolute h-auto right-[7%] w-[300px] -bottom-[40%] sm:right-[5%] sm:-bottom-[40%] md:right-[3%] md:-bottom-16 lg:right-14 lg:-bottom-4' priority={false} src={doggy} alt='doggy doggy' />
+      </div>
     </div>
   )
 }
